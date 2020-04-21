3c9465e0b94e1c55086c8f7fffd92c2c80deaeff
Fastcampus-unity <jjlee@fastcampus.co.kr>
2020-04-21 23:41:04 +0900

    Chapter 04-02. 적 비행기의 이동과 충돌 감지하기

--- FirstWinger/Assets/Scripts/Enemy.cs
+++ FirstWinger/Assets/Scripts/Enemy.cs
@@ -40,8 +40,15 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     float CurrentSpeed;
 
+    /// <summary>
+    /// 방향을 고려한 속도 벡터
+    /// </summary>
+    Vector3 CurrentVelocity;
+
+    float MoveStartTime = 0.0f; // 이동시작 시간
 
 
+    float BattleStartTime = 0.0f;   // 전투 시작 시간
 
     // Start is called before the first frame update
     void Start()
@@ -52,35 +59,103 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            Appear(new Vector3(7, transform.position.y, transform.position.z));
+        }
+        //
+        switch (CurrentState)
+        {
+            case State.None:
+            case State.Ready:
+            case State.Dead:
+                return;
+            case State.Appear:
+            case State.Disappear:
+                UpdateSpeed();
+                UpdateMove();
+                break;
+            case State.Battle:
+                UpdateBattle();
+                break;
+            default:
+                Debug.LogError("Undefined State!");
+                break;
+        }
     }
 
     void UpdateSpeed()
     {
+        // CurrentSpeed 에서 MaxSpeed 에 도달하는 비율을 흐른 시간많큼 계산
+        CurrentSpeed = Mathf.Lerp(CurrentSpeed, MaxSpeed, (Time.time - MoveStartTime) / MaxSpeedTime);
     }
 
     void UpdateMove()
     {
+        float distance = Vector3.Distance(TargetPosition, transform.position);
+        if (distance == 0)
+        {
+            Arrived();
+            return;
+        }
+
+        // 이동벡터 계산. 양 벡터의 차를 통해 이동벡터를 구한후 nomalized 로 단위벡터를 구한다. 속도를 곱해 현재 이동할 벡터를 계산
+        CurrentVelocity = (TargetPosition - transform.position).normalized * CurrentSpeed;
+
+        // 자연스러운 감속으로 목표지점에 도착할 수 있도록 계산
+        // 속도 = 거리 / 시간 이므로 시간 = 거리/속도
+        transform.position = Vector3.SmoothDamp(transform.position, TargetPosition, ref CurrentVelocity, distance / CurrentSpeed, MaxSpeed);
+    }
+
+    void UpdateBattle()
+    {
+        // 임시로 3초 대기후 퇴장
+        if (Time.time - BattleStartTime > 3.0f)
+        {
+            Disappear(new Vector3(-15, transform.position.y, transform.position.z));
+        }
     }
 
     void Arrived()
     {
-
+        CurrentSpeed = 0.0f;    // 도착했으므로 속도는 0
+        if (CurrentState == State.Appear)
+        {
+            CurrentState = State.Battle;
+            BattleStartTime = Time.time;
+        }
+        else if (CurrentState == State.Disappear)
+            CurrentState = State.None;
     }
 
     public void Appear(Vector3 targetPos)
     {
         TargetPosition = targetPos;
-        CurrentSpeed = MaxSpeed;
+        CurrentSpeed = MaxSpeed;    // 나타날때는 최고 스피드로 설정
 
         CurrentState = State.Appear;
+        MoveStartTime = Time.time;
     }
 
     void Disappear(Vector3 targetPos)
     {
         TargetPosition = targetPos;
-        CurrentSpeed = 0.0f;
+        CurrentSpeed = 0.0f;           // 사라질때는 0부터 속도 증가
 
         CurrentState = State.Disappear;
+        MoveStartTime = Time.time;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.GetComponentInParent<Player>();
+        if (player)
+            player.OnCrash(this);
+    }
+
+    public void OnCrash(Player player)
+    {
+        Debug.Log("OnCrash player = " + player);
+    }
+
 }
--- FirstWinger/Assets/Scripts/Player.cs
+++ FirstWinger/Assets/Scripts/Player.cs
@@ -64,4 +64,16 @@ public class Player : MonoBehaviour
 
         return moveVector;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Enemy enemy = other.GetComponentInParent<Enemy>();
+        if (enemy)
+            enemy.OnCrash(this);
+    }
+
+    public void OnCrash(Enemy enemy)
+    {
+        Debug.Log("OnCrash enemy = " + enemy);
+    }
 }
