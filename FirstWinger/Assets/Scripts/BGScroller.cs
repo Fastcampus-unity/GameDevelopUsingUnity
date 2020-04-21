0db11ab74113e7282b6775f2a4667874c97e4e8d
DARKERMAINCOM\USER <darker76@gmail.com>
2020-04-21 15:26:32 +0900

    Chapter 03-01. 게임의 배경 만들기

---
+++ FirstWinger/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BGScrollData
+{
+    public Renderer RenderForScroll;
+    public float Speed;
+    public float OffsetX;
+
+}
+
+public class BGScroller : MonoBehaviour
+{
+    [SerializeField]
+    BGScrollData[] ScrollDatas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateScroll();
+    }
+
+    void UpdateScroll()
+    {
+        for(int i = 0; i < ScrollDatas.Length; i++)
+        {
+            SetTextureOffset(ScrollDatas[i]);
+        }
+        
+    }
+
+    void SetTextureOffset(BGScrollData scrollData)
+    {
+        scrollData.OffsetX += (float)(scrollData.Speed) * Time.deltaTime;
+        if (scrollData.OffsetX > 1)
+            scrollData.OffsetX = scrollData.OffsetX % 1.0f;
+
+        Vector2 Offset = new Vector2(scrollData.OffsetX, 0);
+
+        scrollData.RenderForScroll.material.SetTextureOffset("_MainTex", Offset);
+    }
+      
+}
