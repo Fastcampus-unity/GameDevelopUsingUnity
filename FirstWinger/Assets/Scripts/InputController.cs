9f5e4c9e1934d7744191abce79ea02d0e1542fd2
DARKERMAINCOM\USER <darker76@gmail.com>
2020-04-21 16:04:47 +0900

    Chapter 03-03. 입력 감지 클래스와 시스템 클래스 만들기

---
+++ FirstWinger/Assets/Scripts/InputController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateInput();
+    }
+
+    void UpdateInput()
+    {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("Up");
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("Down");
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("Left");
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("Right");
+        }
+
+    }
+}
---
+++ FirstWinger/Assets/Scripts/SystemManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemManager : MonoBehaviour
+{
+    static SystemManager intance = null;
+
+    public static SystemManager Instance
+    {
+        get
+        {
+            return intance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(intance != null)
+        {
+            Debug.LogError("SystemManager error! Singletone eror!");
+            Destroy(gameObject);
+            return;
+        }
+
+        intance = this;
+    }
+    //
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
