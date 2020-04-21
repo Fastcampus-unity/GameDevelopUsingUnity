7b39db9be06cad7c2fc428fcc9b4a92d6c4f1dfb
DARKERMAINCOM\USER <darker76@gmail.com>
2020-04-21 15:54:32 +0900

    Chapter 03-02. 플레이어 만들기

---
+++ FirstWinger/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 MoveVector = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMove();
+    }
+
+    void UpdateMove()
+    {
+        Vector3 move = MoveVector;
+
+        transform.position += MoveVector;
+    }
+
+    void ProcessInput(Vector3 moveDirection)
+    {
+        MoveVector = moveDirection * 1.0f;
+    }
+}
