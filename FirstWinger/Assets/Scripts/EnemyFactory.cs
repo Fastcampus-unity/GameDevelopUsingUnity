0f6cca1c55c6dd4b08d031a817f82657c3f94ebc
Fastcampus-unity <jjlee@fastcampus.co.kr>
2020-04-21 23:51:19 +0900

    Chapter 04-03. 적 비행기를 Prefab파일로 저장하기

---
+++ FirstWinger/Assets/Scripts/EnemyFactory.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFactory : MonoBehaviour
+{
+    public const string EnemyPath = "Prefabs/Enemy";
+
+    Dictionary<string, GameObject> EnemyFileCache = new Dictionary<string, GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public GameObject Load(string resourcePath)
+    {
+        GameObject go = null;
+        if (EnemyFileCache.ContainsKey(resourcePath))   // 캐시 확인
+        {
+            go = EnemyFileCache[resourcePath];
+        }
+        else
+        {
+            // 캐시에 없으므로 로드
+            go = Resources.Load<GameObject>(resourcePath);
+            if (!go)
+            {
+                Debug.LogError("Load error! path = " + resourcePath);
+                return null;
+            }
+            // 로드 후 캐시에 적재
+            EnemyFileCache.Add(resourcePath, go);
+        }
+
+        // 인스턴스를 해서 반환
+        GameObject InstancedGO = Instantiate<GameObject>(go);
+        return InstancedGO;
+    }
+
+}
