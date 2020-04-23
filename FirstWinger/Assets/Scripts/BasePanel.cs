33cec466727f645594cfcf49d3a0892d8d300fe1
Fastcampus-unity <jjlee@fastcampus.co.kr>
2020-04-23 17:57:25 +0900

    Chapter 07-04. 패널 클래스와 매니저 클래스 제작하기

---
+++ FirstWinger/Assets/Scripts/BasePanel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePanel : MonoBehaviour
+{
+    private void Awake()
+    {
+        InitializePanel();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        DestroyPanel();
+    }
+
+    public virtual void InitializePanel()
+    {
+        PanelManager.RegistPanel(GetType(), this);
+    }
+
+    public virtual void DestroyPanel()
+    {
+        PanelManager.UnregistPanel(GetType());
+    }
+
+    public virtual void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public virtual void Close()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
---
+++ FirstWinger/Assets/Scripts/PanelManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelManager : MonoBehaviour
+{
+    static Dictionary<Type, BasePanel> Panels = new Dictionary<Type, BasePanel>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static bool RegistPanel(Type PanelClassType, BasePanel basePanel)
+    {
+        if (Panels.ContainsKey(PanelClassType))
+        {
+            Debug.LogError("RegistPanel Error! Already exist Type! PanelClassType = " + PanelClassType.ToString());
+            return false;
+        }
+
+        Debug.Log("RegistPanel is called! Type = " + PanelClassType.ToString() + ", basePanel = " + basePanel.name);
+
+        Panels.Add(PanelClassType, basePanel);
+        return true;
+    }
+
+    public static bool UnregistPanel(Type PanelClassType)
+    {
+        if (!Panels.ContainsKey(PanelClassType))
+        {
+            Debug.LogError("UnregistPanel Error! Can't Find Type! PanelClassType = " + PanelClassType.ToString());
+            return false;
+        }
+
+        Panels.Remove(PanelClassType);
+        return true;
+    }
+
+    public static BasePanel GetPanel(Type PanelClassType)
+    {
+        if(!Panels.ContainsKey(PanelClassType))
+        {
+            Debug.LogError("GetPanel Error! Can't Find Type! PanelClassType = " + PanelClassType.ToString());
+            return null;
+        }
+
+        return Panels[PanelClassType];
+    }
+}
