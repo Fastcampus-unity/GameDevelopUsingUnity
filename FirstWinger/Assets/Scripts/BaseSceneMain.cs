45c1bcbd93bd62a461f439c5fe37d8b62119fcd6
Fastcampus-unity <jjlee@fastcampus.co.kr>
2020-04-24 11:40:51 +0900

    Chapter 09-02. 로딩 화면 제작하기

--- FirstWinger/Assets/Scripts/BaseSceneMain.cs
+++ FirstWinger/Assets/Scripts/BaseSceneMain.cs
@@ -18,7 +18,7 @@ public class BaseSceneMain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateScene();
     }
 
     void OnDestroy()
---
+++ FirstWinger/Assets/Scripts/LadingSceneMain.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LadingSceneMain : BaseSceneMain
+{
+    const float TextUpdateIntaval = 0.15f;
+    const string LoadingTextValue = "Loading...";
+
+    [SerializeField]
+    Text LoadingText;
+
+    int TextIndex = 0;
+    float LastUpdateTime;
+
+    protected override void UpdateScene()
+    {
+        base.UpdateScene();
+
+        float currentTime = Time.time;
+        if(currentTime - LastUpdateTime > TextUpdateIntaval)
+        {
+
+            LoadingText.text = LoadingTextValue.Substring(0, TextIndex + 1);
+
+            TextIndex++;
+            if(TextIndex >= LoadingTextValue.Length)
+            {
+                TextIndex = 0;
+            }
+
+
+            LastUpdateTime = currentTime;
+        }
+
+        //
+    }
+}
